21ae9b2de8656bf6dc68ca5a351a90ce5a79fa25
Xavier Salazar <x.salazar1729@gmail.com>
2021-12-19 22:25:55 -0800

    Add tabs to filter main grid by category

--- src/Components/emoji-grid.tsx
+++ src/Components/emoji-grid.tsx
@@ -1,26 +1,46 @@
 import React from "react";
-import Emoji, { EmojiDatasource, EmojiVariation } from "./emoji";
+import Emoji, { EmojiDatasource } from "./emoji";
 import emojiDatasource from "emoji-datasource/emoji_pretty.json";
-import { Container, ImageList } from "@mui/material";
+import { Container, ImageList, Tab, Tabs, Box } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
 
 interface GridProps {}
 
 interface GridState {
+  sortedEmojis: Array<EmojiDatasource>;
   emojis: Array<EmojiDatasource>;
+  emojiCategories: Array<string>;
+  selectedTab: number;
 }
 
 export default class EmojiGrid extends React.Component<GridProps, GridState> {
   constructor(props: GridProps) {
     super(props);
 
+    var selectedTab = 0;
+
+    var sortedEmojis = emojiDatasource.sort((e1, e2) => {
+      return e1.sort_order > e2.sort_order ? 1 : -1;
+    });
+
+    var emojiCategories = sortedEmojis
+      .map((e: EmojiDatasource) => e.category)
+      .filter((category: string, index: number, self: Array<string>) => {
+        return self.indexOf(category) === index;
+      });
+
+    var emojis = sortedEmojis.filter((e: EmojiDatasource) => {
+      return e.category === emojiCategories[selectedTab];
+    });
+
     this.state = {
-      emojis: emojiDatasource
-        .sort((e1, e2) => {
-          return e1.sort_order > e2.sort_order ? 1 : -1;
-        })
-        .slice(0, 100),
+      sortedEmojis,
+      emojis,
+      emojiCategories,
+      selectedTab,
     };
+
+    this.handleSelectedTabChanged = this.handleSelectedTabChanged.bind(this);
   }
 
   render() {
@@ -30,30 +50,54 @@ export default class EmojiGrid extends React.Component<GridProps, GridState> {
         <Emoji key={uuidv4()} codepoint={e.unified} emoji={e} />
       );
 
-      if (e.skin_variations) {
-        Object.values(e.skin_variations).forEach(
-          (variation: EmojiVariation | undefined) => {
-            if (variation) {
-              emojiToRender.push(
-                <Emoji
-                  key={uuidv4()}
-                  codepoint={variation.unified}
-                  emoji={e}
-                  variation={variation}
-                />
-              );
-            }
-          }
-        );
-      }
+      // if (e.skin_variations) {
+      //   Object.values(e.skin_variations).forEach(
+      //     (variation: EmojiVariation | undefined) => {
+      //       if (variation) {
+      //         emojiToRender.push(
+      //           <Emoji
+      //             key={uuidv4()}
+      //             codepoint={variation.unified}
+      //             emoji={e}
+      //             variation={variation}
+      //           />
+      //         );
+      //       }
+      //     }
+      //   );
+      // }
     });
 
     return (
-      <Container maxWidth="sm">
-        <ImageList cols={8} gap={8}>
-          {emojiToRender}
-        </ImageList>
-      </Container>
+      <div>
+        <Container maxWidth="sm">
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={this.state.selectedTab}
+              onChange={this.handleSelectedTabChanged}
+              variant="scrollable"
+            >
+              {this.state.emojiCategories.map((category: string) => {
+                return <Tab label={category}></Tab>;
+              })}
+            </Tabs>
+          </Box>
+          <ImageList cols={8} gap={8}>
+            {emojiToRender}
+          </ImageList>
+        </Container>
+      </div>
     );
+  }
+
+  handleSelectedTabChanged(event: React.SyntheticEvent, selectedTab: number) {
+    var emojis = this.state.sortedEmojis.filter((e: EmojiDatasource) => {
+      return e.category === this.state.emojiCategories[selectedTab];
+    });
+
+    this.setState({
+      emojis,
+      selectedTab,
+    });
   }
 }
--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,8 @@
-import { CssBaseline } from "@mui/material";
 import EmojiGrid from "./Components/emoji-grid";
 
 export default function App() {
   return (
     <div>
-      <CssBaseline />
       <EmojiGrid />
     </div>
   );
