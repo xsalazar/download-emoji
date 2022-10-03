54c42e09889498c61243d82863024ec41447c74f
Xavier Salazar <x.salazar1729@gmail.com>
2022-10-02 19:09:57 -0700

    React v18 upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./app";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
